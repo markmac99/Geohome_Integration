9f254ac8db6028285d5aa8da33bdcd5b6351d16a
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-12-28 19:34:00 +0000

    Capture live and periodic data to file

--- geohome.py
+++ geohome.py
@@ -4,40 +4,41 @@ Script to intergrate GeoTogether meter readings into the Openhab
 Still needs some work to handle errors
 
 """
-import requests, json
+import requests
+import json
 import threading
 import time
 from datetime import datetime
-from openhab import OpenHAB
+#from openhab import OpenHAB
 import os
 
-BASE_URL='https://api.geotogether.com/'
-LOGIN_URL='usersservice/v2/login'
-DEVICEDETAILS_URL='api/userapi/v2/user/detail-systems?systemDetails=true'
+BASE_URL = 'https://api.geotogether.com/'
+LOGIN_URL = 'usersservice/v2/login'
+DEVICEDETAILS_URL = 'api/userapi/v2/user/detail-systems?systemDetails=true'
 LIVEDATA_URL = 'api/userapi/system/smets2-live-data/'
 PERIODICDATA_URL = 'api/userapi/system/smets2-periodic-data/'
-USERNAME= 'xxxxxxx'
-PASSWORD = 'xxxxxxx'
-LOG_DIRECTORY = 'xxxxxxxxxx''
+USERNAME = 'mark.jm.mcintyre@cesmail.net'
+PASSWORD = 'Wombat33geo!'
+LOG_DIRECTORY = './logs/'
 
 
-
-openhab_URL = 'xxxxxxxxxxxx
-openhab = OpenHAB(openhab_URL)
-HouseElectricityPower = openhab.get_item('HouseElectricityPower')
-HouseGasMeterReading = openhab.get_item('HouseGasMeterReading')
-HouseGasPower = openhab.get_item('HouseGasPower')
+#openhab_URL = 'xxxxxxxxxxxx'
+#openhab = OpenHAB(openhab_URL)
+#HouseElectricityPower = openhab.get_item('HouseElectricityPower')
+#HouseGasMeterReading = openhab.get_item('HouseGasMeterReading')
+#HouseGasPower = openhab.get_item('HouseGasPower')
 
 class GeoHome(threading.Thread):
-    
-    # Thread class with a _stop() method. 
+
+    # Thread class with a _stop() method.
     # The thread itself has to check
     # regularly for the stopped() condition.
-  
+
     def __init__(self, varUserName, varPassword):
-        
+
         log = "Start Intalising: " + str(datetime.now())
-        super(MyThread, self).__init__()
+        threading.Thread.__init__(self)
+        #super(Thread, self).__init__()
         self._stop = threading.Event()
         self.varUserName = varUserName
         self.varPassword = varPassword
@@ -46,72 +47,104 @@ class GeoHome(threading.Thread):
         self.authorise()
         self.getDevice()
         log = log + os.linesep + "End Intalising: " + str(datetime.now())
-        with open(LOG_DIRECTORY+"GeoHomeLog"+time.strftime("%Y%m%d")+".json",mode='a+', encoding='utf-8') as f:
-            f.write(log + os.linesep)   
-  
+        with open(LOG_DIRECTORY+"GeoHomeLog"+time.strftime("%Y%m%d")+".json", mode='a+', encoding='utf-8') as f:
+            f.write(log + os.linesep)
+        self.ttlcountdown = 0
+
     def authorise(self):
-       data = { 'identity' : self.varUserName , 'password' : self.varPassword }
-       r=requests.post(BASE_URL+LOGIN_URL, data=json.dumps(data), verify=False)
-       authToken = json.loads(r.text)['accessToken']
-       self.headers = {"Authorization": "Bearer " + authToken}
-       return
-   
-    def getDevice(self):
-        r=requests.get(BASE_URL+DEVICEDETAILS_URL, headers=self.headers)
-        self.deviceId = json.loads(r.text)['systemRoles'][0]['systemId']
-        print( 'Device Id:' + self.deviceId)
+        data = {'identity': self.varUserName, 'password': self.varPassword}
+        r = requests.post(BASE_URL+LOGIN_URL,
+                          data=json.dumps(data), verify=False)
+        authToken = json.loads(r.text)['accessToken']
+        self.headers = {"Authorization": "Bearer " + authToken}
         return
-  
+
+    def getDevice(self):
+        r = requests.get(BASE_URL+DEVICEDETAILS_URL, headers=self.headers)
+        self.deviceId = json.loads(r.text)['systemRoles'][0]['systemId']
+        print('Device Id:' + self.deviceId)
+        return
+
     # function using _stop function
     def stop(self):
         self._stop.set()
-  
+
     def stopped(self):
         return self._stop.isSet()
-  
+
     def run(self):
         while True:
             if self.stopped():
                 return
-            
-            log ="Start Api Call: " + str(datetime.now())
-            
-            r=requests.get(BASE_URL+LIVEDATA_URL+ self.deviceId, headers=self.headers)
+
+            log = "Start Api Call: " + str(datetime.now())
+
+            r = requests.get(BASE_URL+LIVEDATA_URL +
+                             self.deviceId, headers=self.headers)
             if r.status_code != 200:
-                #Not sucesful. Assume Authentication Error
-                log = log + os.linesep + "Request Status Error:" + str(r.status_code)
-                #Reauthenticate. Need to add more error trapping here in case api goes down.
+                # Not successful. Assume Authentication Error
+                log = log + os.linesep + \
+                    "Request Status Error:" + str(r.status_code)
+                # Reauthenticate. Need to add more error trapping here in case api goes down.
                 self.authorise()
                 self.getDevice()
-            else:    
+            else:
                 log = log + os.linesep + json.dumps(r.text)
-                power_dict =json.loads(r.text)['power']
-                #Trye and find the electrivity usage
+                power_dict = json.loads(r.text)['power']
+                outfnam = datetime.now().strftime('live-%Y%m.json')
+                with open(outfnam, 'a') as outf:
+                    outf.write('{}\n'.format(r.text))
+
+                # Try to find the electrivity usage
                 try:
-                    Electrcity_usage=([x for x in power_dict if x['type'] == 'ELECTRICITY'][0]['watts'])
+                    Electrcity_usage = (
+                        [x for x in power_dict if x['type'] == 'ELECTRICITY'][0]['watts'])
                 except:
                     # Cant find Electricity in list. Add to log file but do nothing else
                     log = log + os.linesep + "No Electricity reading found"
                 else:
-                    #Code executed ok so update the usage
-                    HouseElectricityPower.state= Electrcity_usage
-                    log = log + os.linesep + "Electrcity_usage:"+str(Electrcity_usage)
-    
+                    # Code executed ok so update the usage
+                    #HouseElectricityPower.state= Electrcity_usage
+                    log = log + os.linesep + \
+                        "Electrcity_usage:"+str(Electrcity_usage)
+
                 try:
-                    Gas_usage=([x for x in power_dict if x['type'] == 'GAS_ENERGY'][0]['watts'])
+                    Gas_usage = (
+                        [x for x in power_dict if x['type'] == 'GAS_ENERGY'][0]['watts'])
                 except:
                     # Cant find Gas in list. Add to log file but do nothing else
                     log = log + os.linesep + "No Gas reading found"
                 else:
-                    #Update Gas reading when built!
+                    # Update Gas reading 
                     log = log + os.linesep + "Gas Usage:" + str(Gas_usage)
-                
-                time.sleep(10)
-            
-            with open(LOG_DIRECTORY+"GeoHomeLog"+time.strftime("%Y%m%d")+".json",mode='a+', encoding='utf-8') as f:
-                f.write(log + os.linesep)    
-            
-  
 
-t1 = GeoHome(USERNAME,PASSWORD)
+            with open(LOG_DIRECTORY+"GeoHomeLog"+time.strftime("%Y%m%d")+".json", mode='a+', encoding='utf-8') as f:
+                f.write(log + os.linesep)
+            if self.ttlcountdown == 0:
+                r = requests.get(BASE_URL+PERIODICDATA_URL +
+                                self.deviceId, headers=self.headers)
+                if r.status_code != 200:
+                    # Not successful. Assume Authentication Error
+                    log = log + os.linesep + \
+                        "Request Status Error:" + str(r.status_code)
+                    # Reauthenticate. Need to add more error trapping here in case api goes down.
+                    self.authorise()
+                    self.getDevice()
+                else:
+                    self.ttlcountdown = json.loads(r.text)['ttl']
+                    outfnam = datetime.now().strftime('peri-%Y%m.json')
+                    with open(outfnam, 'a') as outf:
+                        outf.write('{}\n'.format(r.text))
+                    print(r.text)
+
+            time.sleep(10)
+            self.ttlcountdown -= 10
+
+
+t1 = GeoHome(USERNAME, PASSWORD)
 t1.start()
+try:
+    while True:
+        time.sleep(5)
+except KeyboardInterrupt:
+    t1.stop()
