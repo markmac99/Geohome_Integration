9aa5ca2a71340e36d55ece2163fce5aed1fd97d1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2021-12-28 20:12:03 +0000

    Basic data analysis

---
+++ analyse_data.py
@@ -0,0 +1,60 @@
+#
+#
+#
+
+import datetime
+import json
+import matplotlib.pyplot as plt
+
+
+def graphLiveData(ym):
+
+    dts = []
+    ele = []
+    gas = []
+    if ym == 0:
+        fname = datetime.datetime.now().strftime('live-%Y%m.json')
+    else:
+        fname = 'live-{}.json'.format(ym)
+    with open (fname,'r') as inf:
+        lis = inf.readlines()
+    for li in lis:
+        dtstamp = datetime.datetime.fromtimestamp(json.loads(li)['latestUtc'])
+        try:
+            Electrcity_usage = (
+                [x for x in power_dict if x['type'] == 'ELECTRICITY'][0]['watts'])
+        except:
+            eleval = 0
+        else:
+            eleval = float(Electrcity_usage)
+        
+        power_dict = json.loads(li)['power']
+        try:
+            Electrcity_usage = (
+                [x for x in power_dict if x['type'] == 'ELECTRICITY'][0]['watts'])
+        except:
+            eleval = 0
+        else:
+            eleval = float(Electrcity_usage)
+        try:
+            gas_usage = (
+                [x for x in power_dict if x['type'] == 'GAS_ENERGY'][0]['watts'])
+        except:
+            gasval = 0
+        else:
+            gasval = float(gas_usage)
+        print(dtstamp.strftime('%Y-%m-%dT%H:%M:%S'), eleval, gasval )
+        if dtstamp > datetime.datetime.fromtimestamp(0): 
+            dts.append(dtstamp)
+            ele.append(eleval)
+            gas.append(gasval)
+
+    plt.plot(dts, ele, label='Electricity')
+    plt.plot(dts, gas, label='Gas')
+    plt.xlabel('Time')
+    plt.ylabel('kW')
+    plt.title('Home Energy Consumption')
+    plt.show()
+
+if __name__ == '__main__':
+    graphLiveData(0)
